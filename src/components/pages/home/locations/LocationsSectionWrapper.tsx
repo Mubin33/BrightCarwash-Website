95c042ec4d6ec772b3b2fd1221f131c4ec3a82f4
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-07-25 16:39:47 +0600

    Updated width

--- src/components/pages/home/locations/LocationsSectionWrapper.tsx
+++ src/components/pages/home/locations/LocationsSectionWrapper.tsx
@@ -13,7 +13,7 @@ export function LocationsSectionWrapper() {
             className={`flex py-10 px-4 md:px-6 lg:px-10 flex-col justify-center items-center gap-12 self-stretch ${isDark ? 'bg-[#1a1712]' : 'bg-[#fff8ee]'}`}
         >
             <LocationsHeader />
-            <div className="flex flex-col gap-6 w-full max-w-7xl xl:max-w-330 2xl:max-w-[1600px]">
+            <div className="flex flex-col gap-6 w-full max-w-7xl xl:max-w-330 ">
                 <div className="flex flex-col lg:flex-row w-full items-stretch gap-6">
                     {locations.map((loc) => (
                         <LocationCard key={loc.name} location={loc} />
