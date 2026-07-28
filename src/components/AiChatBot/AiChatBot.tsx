63afaf6cb79a5585b2f1d6f2cca3584502bace59
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-07-28 09:43:09 +0600

    Design updated

--- src/components/AiChatBot/AiChatBot.tsx
+++ src/components/AiChatBot/AiChatBot.tsx
@@ -7,10 +7,10 @@ export default function AiChatBot() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="fixed bottom-4 right-4 z-50">
+    <div className="fixed bottom-6 right-6 z-50">
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="flex items-center justify-center gap-2 bg-[#FEC300] hover:bg-[#fcd824] duration-300 p-3 rounded-xl fixed bottom-4 right-4 z-40 text-base lg:text-xl cursor-pointer"
+        className="flex items-center justify-center gap-2 bg-[#FEC300] hover:bg-[#fcd824] duration-300 p-3 rounded-xl fixed bottom-6 right-6 z-40 text-base lg:text-xl cursor-pointer shadow-[0_5.6px_5.6px_0_rgba(0,0,0,0.25)]"
       >
         <div>
           <BotIcon />
