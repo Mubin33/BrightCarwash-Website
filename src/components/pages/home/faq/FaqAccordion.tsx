71e0b874ef4f044be3994677b8d6ccdd370e6ad1
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-08-09 09:51:04 +0600

    Fix classes and typography

--- src/components/pages/home/faq/FaqAccordion.tsx
+++ src/components/pages/home/faq/FaqAccordion.tsx
@@ -17,16 +17,16 @@ export function FaqAccordion({ item }: Props) {
     return (
         <div
             className={`self-stretch p-6 rounded-lg inline-flex justify-between items-start cursor-pointer transition-colors ${isOpen
-                ? 'bg-yellow-50 outline outline-[1.50px] outline-offset-[-1.50px] outline-yellow-400'
+                ? 'bg-yellow-50 outline outline-offset-[-1.50px] outline-yellow-400'
                 : isDark
-                    ? 'bg-white/[0.04] outline outline-1 outline-offset-[-1px] outline-white/20'
-                    : 'bg-white outline outline-1 outline-offset-[-1px] outline-zinc-200'
+                    ? 'bg-white/4 outline-1 -outline-offset-1 outline-white/20'
+                    : 'bg-white outline-1 -outline-offset-1 outline-zinc-200'
                 }`}
             onClick={() => setIsOpen(!isOpen)}
         >
             <div className="flex-1 flex flex-col justify-start items-start gap-2">
                 <div
-                    className={`justify-start text-2xl font-normal font-['Bebas_Neue'] leading-6 ${isOpen
+                    className={`justify-start text-2xl font-normal font-['Bebas_Neue'] leading-6 tracking-wide ${isOpen
                         ? 'text-neutral-800'
                         : isDark
                             ? 'text-white'
