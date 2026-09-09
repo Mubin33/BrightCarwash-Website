863650ea9dfcc0ac3dffd11872ea52739dda2938
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-09-09 10:56:00 +0600

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "clan-figure-breakdown-raymond.trycloudflare.com",
+        hostname: "seattle-relating-background-boating.trycloudflare.com",
         pathname: "/**",
       },
       {
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "clan-figure-breakdown-raymond.trycloudflare.com",
+        hostname: "seattle-relating-background-boating.trycloudflare.com",
         pathname: "/**",
       },
     ],
